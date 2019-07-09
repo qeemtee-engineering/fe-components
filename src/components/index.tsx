bca4cfc1187361866e13ade78664d76f32b4e206
Ali Raza Sherazi <syedd.sherazi@gmail.com>
2019-07-09 20:33:11 +0500

    minor change

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,5 @@
 export { default as Button } from './Button';
 export { default as Modal } from './Modal';
 export { default as Table } from './Table';
+export { default as TypeAhead } from './TypeAhead';
+
