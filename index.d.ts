dc44758d3517d47eca126c497a1112533fe54ff8
Ali Raza Sherazi <syedd.sherazi@gmail.com>
2019-07-09 20:44:41 +0500

    minor change

--- index.d.ts
+++ index.d.ts
@@ -3,3 +3,4 @@ export function SVG(props: ISVG): any;
 export function Modal(props: IModal): any;
 export function Button(props: IButton): any;
 export function Table(props: ITable): any;
+export class TypeAhead {};
