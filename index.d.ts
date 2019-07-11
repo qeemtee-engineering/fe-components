78310065f6d27ad608ae8d7bf2784e118eba1530
Faizan <baigfaizan95@gmail.com>
2019-07-11 16:51:17 +0530

    added input and form validation

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,7 @@
-import { ISVG, IModal, IButton, ITable } from './src/interfaces';
+import { ISVG, IModal, IButton, ITable, IInput } from './src/interfaces';
 export function SVG(props: ISVG): any;
 export function Modal(props: IModal): any;
 export function Button(props: IButton): any;
 export function Table(props: ITable): any;
+export function Input(props: IInput): any;
+export function useForm(validate: (values: object) => object): any;
---
+++ src/components/useForm/validate.tsx
@@ -0,0 +1,50 @@
+function capitalize(value: string) {
+  return `${value.charAt(0).toUpperCase()}${value.slice(1)}`;
+}
+
+export default function validate(
+  value: any,
+  type: any,
+  required: boolean = true,
+  options?: {}
+) {
+  let pattern: any;
+
+  if ((required && (typeof value === 'string' && !value.trim())) || !value) {
+    return `${capitalize(type)} is required.`;
+  }
+
+  switch (type) {
+    case 'email':
+      pattern = /^([0-9a-zA-Z]([-.+\w]*[0-9a-zA-Z])*@(([0-9a-zA-Z])+([-\w]*[0-9a-zA-Z])*\.)+[a-zA-Z]{2,9})$/;
+      if (!pattern.test(value)) {
+        return 'Email is invalid.';
+      }
+      return '';
+
+    case 'password':
+      pattern = /^((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,}))/;
+      if (!pattern.test(value)) {
+        return 'Password is weak. Use at least 8 characters, one lowercase, one uppercase letter and a number.';
+      }
+      return '';
+
+    case 'name':
+      pattern = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/;
+      if (!pattern.test(value)) {
+        return 'Not a valid name';
+      }
+      return '';
+
+    case 'mobile':
+      pattern = /^([0|\+[0-9]{1,5})?([0-9]{10})$/;
+      if (!pattern.test(value)) {
+        return 'Mobile number invalid';
+      }
+
+      return '';
+
+    default:
+      return '';
+  }
+}
