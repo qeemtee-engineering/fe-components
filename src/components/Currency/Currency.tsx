51d7d05404c7523b90ba524a3a0de18e6f21f9fb
Ali Raza Sherazi <syedd.sherazi@gmail.com>
2019-10-17 02:20:37 +0500

    Currency comp updated

--- src/components/Currency/Currency.tsx
+++ src/components/Currency/Currency.tsx
@@ -6,9 +6,10 @@ import './Currency.scss';
 import { Number } from '../';
 
 const Currency = React.forwardRef((props: ICurrency, ref: any) => {
-  const { className, symbol, value, fixed, side, delimiter, ...other } = props;
+  const { className, symbol, symbolClassName, value, fixed, side, delimiter, ...other } = props;
 
   const classes = cs('Currency', className);
+  const symbolClasses = cs('symbol right ', symbolClassName);
 
   const _symbol = symbol || '฿';
 
@@ -16,7 +17,7 @@ const Currency = React.forwardRef((props: ICurrency, ref: any) => {
     <span className={classes}>
       {side === 'left' && <span className="symbol left">{_symbol}</span>}
       <Number {...other} value={value} fixed={fixed} delimiter={delimiter} ref={ref} />
-      {side !== 'left' && <span className="symbol right">{_symbol}</span>}
+      {side !== 'left' && <span className={symbolClasses}>{_symbol}</span>}
     </span>
   );
 });
--- src/interfaces/currency.interface.ts
+++ src/interfaces/currency.interface.ts
@@ -2,5 +2,6 @@ import { INumber } from './number.interface';
 
 export interface ICurrency extends INumber {
   symbol?: string;
+  symbolClassName?: string;
   side?: 'left' | 'right';
 }
